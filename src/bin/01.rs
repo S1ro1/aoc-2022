47a00d196c29a01d9da28d83dd10fad34d58197b
S1ro1 <matej.sirovatka@gmail.com>
2022-12-01 15:32:23 +0100

    Add first day

---
+++ src/bin/01.rs
@@ -0,0 +1,34 @@
+pub fn part_one(input: &str) -> Option<u32> {
+    input.split("\n\n").map(|elf| elf.split('\n').map(|num| num.parse::<u32>().unwrap_or(0))).map(|line| line.sum()).max()
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut arr: Vec<u32> = input.split("\n\n").map(|elf| elf.split('\n').map(|num| num.parse().unwrap_or(0))).map(|line| line.sum()).collect();
+
+    arr.sort();
+
+    arr.windows(3).map(|c| c.into_iter().sum()).max()
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 1);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 1);
+        assert_eq!(part_one(&input), None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 1);
+        assert_eq!(part_two(&input), None);
+    }
+}
